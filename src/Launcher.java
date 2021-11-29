0a6cf1f7bd2fd3f84c440a6f8ff1d81ed319bc77
Costiou Rozenn <rozenn.costiou@orange.fr>
2021-11-29 15:00:55 +0100

    fibo dans launcher

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,14 +3,43 @@ import java.util.Scanner;
 public class Launcher {
 	
 	static Scanner scanner = new Scanner(System.in);
+	static Scanner scanner2 = new Scanner(System.in);
 	
 	public static void main(String args[]){  
 		System.out.println("Bienvenu!");  
 		String saisie = scanner.nextLine();
 		while (!saisie.equals("quit")) {
-			System.out.println("Unknown command");
+			if (saisie.equals("fibo")) {
+				int fibo = fibonacci();
+				System.out.println("Le resultat est : " + fibo);
+			}else {
+				System.out.println("Unknown command");
+			}
 			saisie = scanner.nextLine();
+			
 		}
 	} 
+	
+	public static int fibonacci() {
+		System.out.println("Saisir votre n ");
+		int saisie = scanner2.nextInt();
+		System.out.println("Calcul de fibo pour n = " + saisie);
+		int i = 0;
+		int fibo0 = 0;
+		int fibo1 = 1;
+		if (saisie == 0) {
+			return fibo0;
+		}
+		if (saisie == 1) {
+			return fibo1;
+		}
+		while (i < saisie) {
+			int fibo = fibo1 + fibo0;
+			fibo0 = fibo1;
+			fibo1 = fibo;
+			i++;
+		}
+		return fibo0;
+	}
 
 }
